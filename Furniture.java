da0eedbfcd25d7e44ce25cf98fdf7ad1f84d43a7
Oleg Grigorenko <40439700+TechnicallyWarlock@users.noreply.github.com>
2021-01-11 20:47:55 +0200

    Final commit
    Added final class, implementing Furniture interface and extending Shelf abstract class. Please don't judge too hard :)

--- Furniture.java
+++ Furniture.java
@@ -11,3 +11,43 @@ abstract class Shelf implements Furniture
   public Boolean getDoorsOpen();
   public int ShelvesAmount();
 }
+
+class BookShelf extends Shelf implements Furniture
+{
+  public void material (String type)
+  {
+    System.out.println("This bookshelf is made of" + type);
+  }
+  public void height (int height)
+  {
+    System.out.println("This bookshelf's height is" + height + "cm");
+  }
+  public void width (int width)
+  {
+    System.out.println("This bookshelf's width is" + width + "cm");
+  }
+   public void length (int length)
+  {
+    System.out.println("This bookshelf's length is" + length + "cm");
+  }
+     
+  public Boolean getDoorsOpen(Boolean doors)
+  {
+    if (doors = true)
+      System.out.println("This bookshelf's doors are open");
+    else 
+      System.out.println("This bookshelf's doors are closed");
+  }
+  
+  public int ShelvesAmount(int shelves)
+  {
+    System.out.println("This bookshelf contains" + shelves + "shelves");
+  }
+  
+  private String BookList (String[] Books)
+  {
+     System.out.println("This bookshelf contains following books");
+    for (String book : Books)
+      System.out.prinln(Book);
+  }
+}
